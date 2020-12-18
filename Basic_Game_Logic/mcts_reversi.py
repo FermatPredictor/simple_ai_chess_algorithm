f1fecc65b70e332e0b856228956a4933a34dab3c
unknown <forclass021117@gmail.com>
2020-12-18 12:00:53 +0800

    do MCTS algorithm

--- Basic_Game_Logic/mcts_reversi.py
+++ Basic_Game_Logic/mcts_reversi.py
@@ -1,20 +1,18 @@
 import sys
 sys.path.append('..') # 添加相對路徑上兩層到sys.path，讓程式找到的模組_package
-from _package._game_theory.alpha_beta_algo import MinimaxABAgent
+from _package._game_theory.mcts_algo import MonteCarloTreeSearch, MonteCarloTreeSearchNode
 
 from array import array
 import cProfile
 
 if __name__=='__main__':
     from Fast_Reversi_Cython.reversi_cython import getValidMoves as moves
-    from Fast_Reversi_Cython.reversi_cython import eval_func as ef
     from Fast_Reversi_Cython.reversi_cython import count_tile
 else:
     from .Fast_Reversi_Cython.reversi_cython import getValidMoves as moves
-    from .Fast_Reversi_Cython.reversi_cython import eval_func as ef
     from .Fast_Reversi_Cython.reversi_cython import count_tile
 
-class AB_ReversiState():
+class MCTS_ReversiState():
     """ 
     優化歷程概要:
     目前這個版本跟一開始的版本相比，
@@ -47,7 +45,7 @@ class AB_ReversiState():
     - makeMove(self, action_key): 根據action_key走棋 
     - unMakeMove(self, action_key): 根據同一個action_key還原棋步，通常用於alpha_beta算法避免創建過多state
     - is_terminal(self): 判斷遊戲是否結束了
-    - winner(self, tile): 遊戲結束的狀況下，判斷贏家是誰(0 for 和棋)
+    - winner(self): 遊戲結束的狀況下，判斷贏家是誰(0 for 和棋)
     - next_turn(self): 若遇到輪空的狀況，換下一個玩家行動
     """
     def __init__(self, board, playerColor):
@@ -56,7 +54,6 @@ class AB_ReversiState():
         self.playerColor = playerColor # int(usually 1 or 2, 0 for empty grid), 下一步換誰下
         self.pass_info = 0 # 0:無pass, 1:黑pass, 2:白pass, 3:黑白均pass，此時game over
         self.eval_mode = 'weight'
-        self.w_board = array('i', sum(self.__get_weight_board(), []))
         
     def isOnBoard(self, r, c, H, W):
         return 0 <= r < H and 0 <= c < W
@@ -82,7 +79,6 @@ class AB_ReversiState():
     
     def getValidMoves(self):
         return moves(self.board, self.height, self.width, self.playerColor, self.pass_info)
-                
         
     def winner(self, tile):
         score = count_tile(self.board, tile)
@@ -90,7 +86,7 @@ class AB_ReversiState():
             return 1
         if score<0:
             return -1
-        return 0   
+        return 0  
     
     def is_terminal(self):
         return self.pass_info == 3
@@ -99,78 +95,19 @@ class AB_ReversiState():
         L = self.board
         step = self.width
         return [list(L[r:r+step]) for r in range(0,len(L),step)]
-    
-    def __spiral_coords(self, board, r1, c1, r2, c2, weights):
-        """
-        遍歷矩陣之外圈，
-        並將角、C位、邊設權重
-        """
-        def Pos(r,c):
-            if r in [r1+1, r2-1] or c in [c1+1,c2-1]:
-                return 1
-            return 2
-                
-        for c in range(c1, c2 + 1):
-            board[r1][c] = weights[Pos(r1,c)]
-        for r in range(r1 + 1, r2 + 1):
-            board[r][c2] = weights[Pos(r,c2)]
-        for c in range(c2 - 1, c1, -1):
-            board[r2][c] = weights[Pos(r2,c)]
-        for r in range(r2, r1, -1):
-            board[r][c1] = weights[Pos(r,c1)]
-        board[r1][c1], board[r1][c2], board[r2][c1], board[r2][c2] = [weights[0]]*4
-    
-    def __get_weight_board(self):
-        """
-        這邊手動調整格子對應的權重分配，
-        對應「角、C位、邊、星位、中央邊、中央」
-        """
-        if self.width<6 and self.height<6:
-            # 棋盤過小，直接數棋子數
-            return [[1]*self.width for _ in range(self.height)]
-            
-        weights = [100,-10,20,-50,-2, 1]
-        w_board = [[weights[5]]*self.width for _ in range(self.height)]
-        self.__spiral_coords(w_board, 0,0,self.height-1, self.width-1, weights[:3])
-        self.__spiral_coords(w_board, 1,1,self.height-2, self.width-2, weights[3:5]+[weights[4]])
-        return w_board
-        
-    def evaluation_function(self, tile):
-        return ef(self.board, tile, self.w_board) if self.eval_mode == 'weight' else count_tile(self.board, tile)
-    
-def test_getValidMoves():
-    """ 測試找合法棋步 """
-    play_color = 1
-    board = [[0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,1,0,0,0,0],
-              [0,0,2,2,2,2,0,0],
-              [0,0,0,1,2,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0]]
-    state = AB_ReversiState(board, play_color)
-    print(state.getValidMoves())
-    
-def test_eval_func():
-    play_color = 1
-    board = [[0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,1,0,0,0,0],
-              [0,0,2,2,2,2,0,0],
-              [0,0,0,1,2,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0]]
-    state = AB_ReversiState(board, play_color)
-    print(state.evaluation_function(play_color))
+
+
 
 def main():
-    """
-    黑白棋套用alpha-beta算法
-    平均每秒可搜索10000個節點以上
-    """
     play_color = 1
+#    board = [[0,2,1,2,0,0,0,0],
+#               [0,0,0,0,0,0,0,0],
+#               [0,0,0,0,0,0,0,0],
+#               [0,0,0,0,0,0,0,0],
+#               [0,0,0,0,0,0,0,0],
+#               [0,0,0,0,0,0,0,0],
+#               [0,0,0,0,0,0,0,0],
+#               [0,0,0,0,0,0,0,0]]
     board = [[0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0],
               [0,0,0,1,0,0,0,0],
@@ -179,35 +116,22 @@ def main():
               [0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0]]
-    
-    # play_color = 2
-    # board = [[2,2,2,2,2,2,2,2],
-    #          [1,1,1,1,2,1,2,2],
-    #          [1,1,2,2,2,2,1,2],
-    #          [0,1,1,2,2,1,2,2],
-    #          [0,1,1,2,1,2,2,2],
-    #          [1,1,2,2,2,2,2,2],
-    #          [1,1,1,1,1,2,0,2],
-    #          [2,2,2,2,2,2,0,0]]
-    
-    # play_color = 1
-    # board =[[0, 2, 2, 2, 0, 1, 2, 2],
-    #          [0, 1, 1, 1, 1, 2, 2, 2],
-    #          [0, 0, 1, 2, 2, 2, 2, 2],
-    #          [0, 1, 1, 1, 2, 2, 2, 0],
-    #          [2, 1, 1, 2, 1, 1, 2, 0],
-    #          [2, 2, 1, 2, 2, 1, 2, 2],
-    #          [2, 2, 1, 1, 2, 2, 1, 0],
-    #          [0, 2, 2, 2, 2, 2, 0, 0]]
 
-    state = AB_ReversiState(board, play_color)
-    #print(state.getValidMoves())
-    AI = MinimaxABAgent(8, play_color, state)
-    result = AI.choose_action()
+#    board = [[2,2,2,2,2,2,2,2],
+#             [1,1,1,1,2,1,2,2],
+#             [1,1,2,2,2,2,1,2],
+#             [0,1,1,2,2,1,2,2],
+#             [0,1,1,2,1,2,2,2],
+#             [1,1,2,2,2,2,2,2],
+#             [1,1,1,1,1,2,0,2],
+#             [2,2,2,2,2,2,0,0]]
+
+    state = MCTS_ReversiState(board, play_color)
+    AI = MonteCarloTreeSearch(MonteCarloTreeSearchNode(state))
+    result = AI.best_action(1000)
+    print(result.action)
     
 if __name__=='__main__':
-    #test_getValidMoves()
-    #test_eval_func()
     #cProfile.run('main()')
     main()
     
--- _package/_game_theory/mcts_algo.py
+++ _package/_game_theory/mcts_algo.py
@@ -1,143 +1,8 @@
 import numpy as np
 from collections import defaultdict
 from copy import deepcopy
-from pprint import pprint
 import time
-
-import cProfile
-
-class ReversiState():
-    """ 
-    player_color : int(usually 1 or 2, 0 for empty grid), 下一步換誰下
-    """
-    def __init__(self, board, playerColor):
-        self.board = board
-        self.height, self.width = len(board), len(board[0])
-        self.playerColor = playerColor
-        self.eval_mode = 'weight'
-        self.pass_info = 0 # 0:無pass, 1:黑pass, 2:白pass, 3:黑白均pass，此時game over 
-        self.w_board = self.__get_weight_board()
-        
-    def isOnBoard(self, r, c, H, W):
-        return 0 <= r < H and 0 <= c < W
-    
-    def makeMove(self, action_key):
-        key, pass_info = action_key
-        if key != 'PASS':
-            for r, c in key:
-                self.board[r][c] = self.playerColor
-            self.pass_info = 0
-        else:
-            self.pass_info = self.pass_info | self.playerColor
-        self.next_turn()
-            
-    def unMakeMove(self, action_key):
-        key, pass_info = action_key
-        self.pass_info = pass_info
-        if key != 'PASS':
-            _r, _c = key[0]
-            self.board[_r][_c] = 0
-            for r, c in key[1:]:
-                self.board[r][c] = self.playerColor
-        self.next_turn()
-    
-    def getValidMoves(self):
-        """
-        回傳字典: {合法棋步座標: 翻轉的對手棋子}
-        算法改進: 
-         As is: 檢查棋盤上的所有格子
-         To be: 先定位自己棋子，往八個方向搜索是否夾到對手棋子
-        效能: 與上一版相比大約快 3 倍
-        """
-        H, W = self.height, self.width
-        tile, opp_tile = self.playerColor , self.opp_color()
-        move_dict = {}
-        dirs = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]] # 定義八個方向
-        for r in range(H):
-            for c in range(W):
-                if self.board[r][c]!=tile:
-                    continue
-                for dr, dc in dirs:
-                    _r, _c, flip = r+dr, c+dc, 0
-                    while self.isOnBoard(_r, _c, H, W) and self.board[_r][_c] == opp_tile:
-                        _r, _c, flip = _r+dr, _c+dc, flip+1
-                    if flip and self.isOnBoard(_r, _c, H, W) and self.board[_r][_c] == 0:
-                        # 夾到對手的棋子，回頭記錄
-                        if (_r, _c) not in move_dict:
-                            move_dict[(_r, _c)] = [(_r, _c)] 
-                        grid_r, gird_c = _r, _c
-                        for i in range(flip):
-                            _r, _c = _r-dr, _c-dc
-                            move_dict[(grid_r, gird_c)].append((_r, _c))
-        move_dict = {k:(v, self.pass_info) for k,v in move_dict.items()}
-        return move_dict if move_dict else {'PASS': ('PASS', self.pass_info)}
-                
-
-                
-    def opp_color(self):
-        return 3^self.playerColor
-    
-    def next_turn(self):
-        self.playerColor = self.opp_color()
-    
-    def __spiral_coords(self, board, r1, c1, r2, c2, weights):
-        """
-        遍歷矩陣之外圈，
-        並將角、C位、邊設權重
-        """
-        def Pos(r,c):
-            if r in [r1+1, r2-1] or c in [c1+1,c2-1]:
-                return 1
-            return 2
-                
-        for c in range(c1, c2 + 1):
-            board[r1][c] = weights[Pos(r1,c)]
-        for r in range(r1 + 1, r2 + 1):
-            board[r][c2] = weights[Pos(r,c2)]
-        for c in range(c2 - 1, c1, -1):
-            board[r2][c] = weights[Pos(r2,c)]
-        for r in range(r2, r1, -1):
-            board[r][c1] = weights[Pos(r,c1)]
-        board[r1][c1], board[r1][c2], board[r2][c1], board[r2][c2] = [weights[0]]*4
-    
-    def __get_weight_board(self):
-        """
-        這邊手動調整格子對應的權重分配，
-        對應「角、C位、邊、星位、中央邊、中央」
-        """
-        if self.width<6 and self.height<6:
-            # 棋盤過小，直接數棋子數
-            return [[1]*self.width for _ in range(self.height)]
-            
-        weights = [100,-10,20,-50,-2, 1]
-        w_board = [[weights[5]]*self.width for _ in range(self.height)]
-        self.__spiral_coords(w_board, 0,0,self.height-1, self.width-1, weights[:3])
-        self.__spiral_coords(w_board, 1,1,self.height-2, self.width-2, weights[3:5]+[weights[4]])
-        return w_board
-        
-    def evaluation_function(self, tile):
-        score, opp = 0, tile^3
-        coef = {0:0, tile: 1, opp:-1} 
-        for r in range(self.height):
-            for c in range(self.width):
-                score += (self.w_board[r][c] if self.eval_mode == 'weight' else 1)* coef[self.board[r][c]]
-        return score
-    
-    def winner(self, tile):
-        score, opp = 0, tile^3
-        coef = {0:0, tile: 1, opp:-1} 
-        for r in range(self.height):
-            for c in range(self.width):
-                score += 1* coef[self.board[r][c]]
-        if score>0:
-            return -1
-        if score<0:
-            return 1
-        return 0  
-        
-    def is_terminal(self):
-        return self.pass_info == 3
-
+import random
 
 class MonteCarloTreeSearchNode(object):
     """
@@ -149,8 +14,7 @@ class MonteCarloTreeSearchNode(object):
     * state: 類似指標可以inplace修改的物件
        - def getValidMoves(self): 回傳一個字典，key值是行動，value是一個action_key，用來走棋或還原state
        - def is_terminal(self): 判斷一場遊戲是否已經結束
-       - def makeMove(self, action_key): 自定義action_key行動
-       - def unMakeMove(self, action_key): 此函數的用意是state可以還原，就只需創建一次，避免需要大量複製state而耗時
+       - def winner(self, tile): 判斷tile的勝負，1是win，-1是lose，0是和局
     """
     def __init__(self, state, parent=None, action=None):
         self._number_of_visits = 0.
@@ -163,23 +27,23 @@ class MonteCarloTreeSearchNode(object):
     @property
     def untried_actions(self):
         if not hasattr(self, '_untried_actions'):
-            self._untried_actions = list(self.state.getValidMoves().values())
+            self._untried_actions = [(k, v) for k, v in self.state.getValidMoves().items()]
         return self._untried_actions
 
     @property
     def q(self):
         wins, loses = self._results[1], self._results[-1]
-        return wins # ins-loses
+        return wins #wins-loses
 
     @property
     def n(self):
         return self._number_of_visits
 
     def expand(self):
-        action = self.untried_actions.pop()
+        key, action = self.untried_actions.pop()
         cur_state = deepcopy(self.state)
         cur_state.makeMove(action)
-        child_node = MonteCarloTreeSearchNode(cur_state, parent=self, action=action)
+        child_node = MonteCarloTreeSearchNode(cur_state, parent=self, action=(key, action))
         self.children.append(child_node)
         return child_node
 
@@ -189,17 +53,17 @@ class MonteCarloTreeSearchNode(object):
     def rollout(self):
         player_color = self.state.playerColor
         cur_state = deepcopy(self.state)
-        print('rollout前')
-        print(player_color)
-        pprint(cur_state.board)
+        #print('rollout前')
+        #print(player_color)
+        #pprint(cur_state.board)
         while not cur_state.is_terminal():
             possible_moves = list(cur_state.getValidMoves().values())
             action = self.rollout_policy(possible_moves)
             cur_state.makeMove(action)
-        print('rollout後')
-        print(player_color)
-        pprint(cur_state.board)
-        return cur_state.winner(player_color)
+        #print('rollout後')
+        #print(player_color)
+        #pprint(cur_state.board)
+        return -cur_state.winner(player_color)
 
     def backpropagate(self, result):
         self._number_of_visits += 1.
@@ -218,7 +82,7 @@ class MonteCarloTreeSearchNode(object):
         return self.children[np.argmax(choices_weights)]
 
     def rollout_policy(self, possible_moves):
-        return possible_moves[np.random.randint(len(possible_moves))]
+        return random.choice(possible_moves)
     
 class MonteCarloTreeSearch:
     def __init__(self, node: MonteCarloTreeSearchNode):
@@ -246,40 +110,3 @@ class MonteCarloTreeSearch:
                 current_node = current_node.best_child()
         return current_node
 
-def main():
-    play_color = 1
-    # board = [[0,2,1,2,0,0,0,0],
-    #           [0,0,0,0,0,0,0,0],
-    #           [0,0,0,0,0,0,0,0],
-    #           [0,0,0,0,0,0,0,0],
-    #           [0,0,0,0,0,0,0,0],
-    #           [0,0,0,0,0,0,0,0],
-    #           [0,0,0,0,0,0,0,0],
-    #           [0,0,0,0,0,0,0,0]]
-    board = [[0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,1,0,0,0,0],
-              [0,0,2,2,2,2,0,0],
-              [0,0,0,1,2,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0]]
-    """
-    board = [[2,2,2,2,2,2,2,2],
-             [1,1,1,1,2,1,2,2],
-             [1,1,2,2,2,2,1,2],
-             [0,1,1,2,2,1,2,2],
-             [0,1,1,2,1,2,2,2],
-             [1,1,2,2,2,2,2,2],
-             [1,1,1,1,1,2,0,2],
-             [2,2,2,2,2,2,0,0]]
-    """
-    state = ReversiState(board, play_color)
-    AI = MonteCarloTreeSearch(MonteCarloTreeSearchNode(state))
-    result = AI.best_action(500)
-    pprint(result.action)
-    
-
-if __name__=='__main__':
-    main()
-    #cProfile.run('main()')
